f9924a0ff97c9a1ac05ee83729990d9d0033319c
RamonPetitinga <ramonlpbahia@gmail.com>
2025-01-18 19:55:27 -0300

    Adicionado código para verificar qual tecla pressionada.

--- keypad_tarefa.c
+++ keypad_tarefa.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/pwm.h" 
-#include "hardware/clocks.h" 
- 
-// Definições das portas GPIO dos LEDs 
-#define GREEN_LED_PIN 11 
-#define BLUE_LED_PIN 12 
-#define RED_LED_PIN 13 
- 
-//Definição da porta GGPIO do BUZZER 
-#define BUZZER_PIN 21 
- 
-// Define os GPIOs para as linhas e colunas do teclado matricial 4x4 
-#define ROWS 4 
-#define COLS 4 
- 
-// Mapear GPIOs para linhas e colunas 
-const uint8_t row_pins[ROWS] = {8, 7, 6, 5}; 
-const uint8_t col_pins[COLS] = {4, 3, 2, 1}; 
- 
-// Matriz de teclas 
-const char key_map[ROWS][COLS] = { 
-    {'1', '2', '3', 'A'}, 
-    {'4', '5', '6', 'B'}, 
-    {'7', '8', '9', 'C'}, 
-    {'*', '0', '#', 'D'} 
-}; 
+#include "hardware/pwm.h"
+#include "hardware/clocks.h"
+
+// Definições das portas GPIO dos LEDs
+#define GREEN_LED_PIN 11
+#define BLUE_LED_PIN 12
+#define RED_LED_PIN 13
+
+// Definição da porta GGPIO do BUZZER
+#define BUZZER_PIN 21
+
+// Define os GPIOs para as linhas e colunas do teclado matricial 4x4
+#define ROWS 4
+#define COLS 4
+
+// Mapear GPIOs para linhas e colunas
+const uint8_t row_pins[ROWS] = {8, 7, 6, 5};
+const uint8_t col_pins[COLS] = {4, 3, 2, 1};
+
+// Matriz de teclas
+const char key_map[ROWS][COLS] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
+
+// Verifica qual tecla foi pressionada
+char scan_keypad()
+{
+    for (int row = 0; row < ROWS; row++)
+    {
+        gpio_put(row_pins[row], 0); // Configura a linha atual como LOW
+        for (int col = 0; col < COLS; col++)
+        {
+            if (gpio_get(col_pins[col]) == 0)
+            {
+                while (gpio_get(col_pins[col]) == 0)
+                    ;
+                gpio_put(row_pins[row], 1);
+                return key_map[row][col];
+            }
+        }
+        gpio_put(row_pins[row], 1); // Restaura a linha para HIGH
+    }
+    return '\0';
+}
